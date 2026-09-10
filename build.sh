e94250d4f8d5dbbd454f70da42325209b23cdba0
lang8 <lang8@noreply>
2026-09-10 19:15:19 -0400

    Give structs C layout so X11 and GL types can be written as L8 types.

--- build.sh
+++ build.sh
@@ -188,6 +188,7 @@ run_examples_selfhost() {
   example_compile_fail "$tool" immparam examples/immparam.l8 'omit bound'
   example_compile_fail "$tool" retbound examples/retbound.l8 'return bound is @new'
   example "$tool" struct examples/struct.l8 '3 12 13'
+  example "$tool" clayout examples/clayout.l8 'YYYYYYYYY'
   example "$tool" aggcopy examples/aggcopy.l8 'YYYYY'
   example "$tool" dynpid examples/dynpid.l8 'Y'
   example "$tool" nestsum examples/nestsum.l8 '1 2 3 9 4 5 6 7'
